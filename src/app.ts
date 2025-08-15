8a244b7c4a0b2b330b32d3bcfe66e525b7e5c66a
NaokiMizuta5 <naoki.mizuta.work@gmail.com>
2025-08-15 18:35:35 +0900

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
-import { serveStatic } from 'hono/cloudflare-workers'
-import type { Bindings, Variables } from './types'
-import { home } from './routes/index'
 import { auth } from './routes/auth'
 import { editor } from './routes/editor'
+import { home } from './routes/index'
 import { post } from './routes/post'
-
+import type { Bindings, Variables } from './types'
 
 const app = new Hono<{ Bindings: Bindings; Variables: Variables }>()
 
@@ -16,14 +14,20 @@ app.use('*', secureHeaders())
 
 // ルーティング
 app.route('/', home)
-app.route('/', auth)
-app.route('/', editor)
-app.route('/', post)
+app.route('/auth', auth)
+app.route('/editor', editor)
+app.route('/post', post)
 
-// debug 
+// debug
 
 // debug
 app.get('/__ping', (c) => c.text('pong'))
-app.get('/__env', (c) => c.json({ hasDB: !!c.env.DB, hasAdminUser: !!c.env.ADMIN_USER, hasSessionSecret: !!c.env.SESSION_SECRET }))
+app.get('/__env', (c) =>
+  c.json({
+    hasDB: !!c.env.DB,
+    hasAdminUser: !!c.env.ADMIN_USER,
+    hasSessionSecret: !!c.env.SESSION_SECRET,
+  }),
+)
 
 export default app
