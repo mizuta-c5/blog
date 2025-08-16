2696e972143f725df00e3536ed48bc2e9c5948d5
NaokiMizuta5 <naoki.mizuta.work@gmail.com>
2025-08-16 12:40:25 +0900

    updated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,33 @@ export const Layout = (title: string, body: any, opts?: { wide?: boolean }) => {
         <title>${title}</title>
         <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
         <link rel="stylesheet" href="/output.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOM3y5iZ9fX1pht6WZ5n6p5+8nR6c5f5f5f5f5f5"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css"
+        />
       </head>
       <body class="max-w-${opts?.wide ? '5xl' : 'xl'} ml-4 mr-4 my-10 px-4 font-sans">
         ${body}
       </body>
+      <footer class="text-center text-sm text-gray-500">
+        <p class="flex justify-center gap-4">
+          <a href="https://github.com/mizuta-c5" class="text-gray-500 hover:text-gray-700"
+            ><i class="fab fa-github text-2xl"></i
+          ></a>
+          <a href="https://x.com/mizuta_c5" class="text-gray-500 hover:text-gray-700"
+            ><i class="fab fa-twitter text-2xl"></i
+          ></a>
+          <a href="https://www.linkedin.com/in/naoki-mizuta-b1b2602a7/" class="text-gray-500 hover:text-gray-700"
+            ><i class="fab fa-linkedin text-2xl"></i
+          ></a>
+        </p>
+      </footer>
     </html>
   `
 }
